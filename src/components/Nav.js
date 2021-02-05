84e3ea12261899c48adcf2953388fd47fab399b1
임관우 <gwanwoo@imgwan-uui-Macmini.local>
2021-02-05 14:36:55 +0900

    13 Nav page completed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 const nav = () => {
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <>
       <div>
-        <Link to="/main"> 로고</Link>
-        <Link to="/signup">회원가입</Link>
-        <Link to="/templogin">비회원</Link>
-        <Link to="/main">메인</Link>
-        <Link to="/mypage">마이페이지</Link>
-        <Link to="/addboard">게시글추가</Link>
-        <Link to="/board">게시글리스트</Link>
-        <Link to="/boarddetail">상세페이지</Link>
+        <Link to="/main">
+          <button>로고</button>
+        </Link>
+        <Link to="/board">
+          <button>보드</button>
+        </Link>
+        <Link to="/mypage">
+          <button>마이페이지</button>
+        </Link>
+        <Link to="/signout">
+          <button>로그아웃</button>
+        </Link>
       </div>
     </>
   );
