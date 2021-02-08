e8339125f6c6193776714b5fff04c04fd291ba0a
Seongseokwon <pam9411@naver.com>
2021-02-08 11:28:24 +0900

    move to detailpage

--- src/components/board/BoardCard.js
+++ src/components/board/BoardCard.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-// import { getBoardDetail } from '../../actions';
+import { getBoardDetail } from '../../actions';
 
 const BoardCard = (props) => {
   console.log(props);
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  // const handleMoveDetailPage = (id) => {
-  // console.log(id);
-  // dispatch(getBoardDetail(id));
-  // props.history.push('/boarddetail');
-  // };
+  const handleMoveDetailPage = (id) => {
+    console.log(id);
+    dispatch(getBoardDetail(id));
+    props.history.push('/boarddetail');
+  };
   return (
     <div onClick={() => handleMoveDetailPage(props.id)}>
       <h2>{props.title}</h2>
